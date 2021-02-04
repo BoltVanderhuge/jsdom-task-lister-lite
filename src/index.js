34e3ec68ee421b357605bf138c05f9fdc3766fb9
john_wisneski <john.j.wisneski@gmail.com>
2021-02-04 18:14:42 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
+const header = document.querySelector('h1')
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form#create-task-form')
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const input = e.target[0].value;
+    const priority = e.target[1].value;
+    console.log(priority)
+    const formLi = document.createElement('li');
+    formLi.contentEditable= "true";
+    formLi.className = "task";
+    const collection = document.querySelector('ul#tasks');
+
+    formLi.innerText = input;
+    if (priority === "Low") {
+      formLi.style.color = "green"
+    } else if (priority === "Medium"){
+      formLi.style.color = "yellow"
+  } else if (priority === "High"){
+      formLi.style.color = "red"
+  };
+    let button = document.createElement('button');
+    button.className = "delete-button";
+    button.innerText = ' 🗑 Delete me!';
+    formLi.appendChild(button);
+
+    // let ebutton = document.createElement('button');
+    // ebutton.className = "edit-button";
+    // ebutton.innerText = ' Edit me!';
+    // formLi.appendChild(ebutton);
+    
+    
+    collection.append(formLi);
+    
+    
+    button.addEventListener('click', function(e){
+      const toDo = e.target.closest('li.task')
+      console.log(toDo)
+      toDo.remove()
+    })
+  })
+
 });
